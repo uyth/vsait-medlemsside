915f619685f688664d58be1127c240950143dc0b
shirajuki <shirajuki00@gmail.com>
2020-12-29 12:38:37 +0100

    #9 #11 Fixed the initial website layout along with login display system on the homepage

--- home/views.py
+++ home/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render, redirect
+from django.utils import timezone
+from django.views import generic
 
-# Create your views here.
+from .forms import LoginForm
+
+# @login_required
+def index(request):
+    context = {}
+    form = LoginForm(data = request.POST)
+    if request.method == "POST":
+        if form.is_valid():
+            user = form.get_user()
+            if (user):
+                login(request,user)
+        return redirect('/')
+    context['form'] = form
+    return render(request, 'home/index.html',context)
