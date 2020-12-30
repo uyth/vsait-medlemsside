6237a5e995e3bdfc7d713e567fca945925a3755b
shirajuki <shirajuki00@gmail.com>
2020-12-30 16:26:06 +0100

    #19 Waiting list system implemented

--- events/models.py
+++ events/models.py
@@ -7,22 +7,30 @@ class Event(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     image_link = models.CharField(max_length=200)
+    #image = models.ImageField()
 
     startTime = models.DateTimeField('startTime')
     endTime = models.DateTimeField('endTime')
     location = models.CharField(max_length=200)
 
-    max_people = models.CharField(max_length=200)
+    max_people = models.IntegerField()
     registrations = models.ManyToManyField(VsaitUser, related_name='registrations', blank=True)
+    waiting_list = models.ManyToManyField(VsaitUser, related_name="waiting_list", blank=True)
 
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.title
     def number_of_registrations(self):
         return self.registrations.count()
+    def number_of_waiting_users(self):
+        return self.waiting_list.count()
     def is_upcoming(self):
         now = timezone.now()
         return self.startTime >= now
+    def is_full(self):
+        return self.registrations.count() >= self.max_people
+    def has_waiting_users(self):
+        return self.waiting_list.count() > 0
     is_upcoming.admin_order_field = 'upcoming'
     is_upcoming.boolean = True
     is_upcoming.short_description = 'Upcoming events'
--- events/views.py
+++ events/views.py
@@ -8,7 +8,7 @@ from .models import Event
 class IndexView(generic.ListView):
     template_name = 'events/index.html'
     context_object_name = 'events'
-    paginate_by = 3
+    paginate_by = 5
 
     def get_queryset(self):
         now = timezone.now()
@@ -22,18 +22,36 @@ class DetailView(generic.DetailView):
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
         event = get_object_or_404(Event, id=self.kwargs['pk'])
-        registered = False
-        if event.registrations.filter(id=self.request.user.id).exists():
-            registered = True
-        data["is_registered"] = registered
+        data["is_registered"] = event.registrations.filter(id=self.request.user.id).exists()
+        data["is_waiting"] = event.waiting_list.filter(id=self.request.user.id).exists()
         data["can_register"] = event.is_upcoming()
         return data
 
 def EventRegistration(request, pk):
     event = get_object_or_404(Event, id=request.POST.get('event_id'))
+    # Does nothing if event startTime has passed
     if event.is_upcoming():
         if event.registrations.filter(id=request.user.id).exists():
+            # Check if user is registered, remove if true
             event.registrations.remove(request.user)
-        else:
+        elif not event.is_full():
+            # Check for fullness, if not full adds user to registrations
             event.registrations.add(request.user)
+        else:
+            # If user already in waiting list, remove
+            if event.waiting_list.filter(id=request.user.id).exists():
+                event.waiting_list.remove(request.user)
+            else:
+                # If user is not in waiting list nor in registration, add to waiting list
+                event.waiting_list.add(request.user)
+        update_waiting_list(event)
     return HttpResponseRedirect(reverse('events:detail', args=[str(pk)]))
+
+# Waiting list update function
+def update_waiting_list(event):
+    # Checks if there's spot on the event, loop through the waiting list then add the user
+    print(event.waiting_list.all())
+    for user in list(event.waiting_list.all()):
+        if event.number_of_registrations() < event.max_people:
+            event.registrations.add(user)
+            event.waiting_list.remove(user)
