d5003121ed3ef307c51c2b6cb9aaa892418c33ca
shirajuki <shirajuki00@gmail.com>
2021-01-07 21:51:53 +0100

    Implement alertpopup confirmation for paying and being a membership

--- home/static/home/profile.js
+++ home/static/home/profile.js
@@ -3,6 +3,7 @@ $(document).ready(() => {
 	// registration form
 	const foodBtn = $("#food").get(0);
 	const passwordBtn = $("#password").get(0);
+	const membershipBtn= $("#membership").get(0);
 	if ($("#alert").get(0)) {
 		// Alert message after password change found
 		const alert = $("#alert").get(0);
@@ -28,6 +29,7 @@ $(document).ready(() => {
 	}
 	$(foodBtn).on('click', click);
 	$(passwordBtn).on('click', click2);
+	$(membershipBtn).on('click', click3);
 });
 
 const click = function(e) {
@@ -76,7 +78,44 @@ const click2 = function(e) {
 		}
 	})
 }
-
+const click3 = function(e) {
+	e.preventDefault();
+	const form = $($(this).parents('form').get(0));
+	const alertInfo = getInfo(this.id);
+	Swal.fire({
+		title: "BLI MEDLEM",
+		html: "Som medlem kan du bli med på arrangementer.<br/><br/>Merk at du også må betale x nok til<br/><br/>1234 12 1234",
+		icon: alertInfo.icon,
+		showCancelButton: true,
+		confirmButtonColor: '#3085d6',
+		cancelButtonColor: '#d33',
+		confirmButtonText: "OK.",
+		input: 'checkbox',
+		inputPlaceholder: 'Jeg har lest.'
+	}).then(function(result) {
+		if (result.value) {
+			Swal.fire({
+				icon: 'success',
+				text: 'SUCCESS MESSAGE MEMBERSHIP ADD!',
+				timer: 1500,
+				showCancelButton: false,
+				showConfirmButton: false,
+			}).then(function() {
+				form.submit();
+			})
+		} else if (result.value === 0) {
+			Swal.fire({
+				icon: 'error',
+				text: "Please make sure to read the text and check the box :(",
+				timer: 1500,
+				showCancelButton: false,
+				showConfirmButton: false,
+			});
+		} else {
+			console.log(`modal was dismissed by ${result.dismiss}`)
+		}
+	})
+}
 const getInfo = function(id) {
 	console.log(id)
 	const info = {}
