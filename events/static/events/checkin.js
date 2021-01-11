46b8f1b2147c38e9dd05ee520ae5f35aef820e11
shirajuki <shirajuki00@gmail.com>
2021-01-11 11:33:59 +0100

    #48 Give users feedback on their attendance registration / checkin.

--- events/static/events/checkin.js
+++ events/static/events/checkin.js
@@ -14,4 +14,31 @@ $(document).ready(() => {
 			cancelButtonText: "Lukk.",
 		})
 	});
+
+	// Success
+	const success = $("p.success").get(0);
+	if (success) {
+		Swal.fire({
+			title: "Success!",
+			text: "We've registered your attendance!",
+			icon: "success",
+			showConfirmButton: true,
+			timer: 1500,
+			confirmButtonColor: '#3085d6',
+			confirmButtonText: "Ok."
+		})
+	}
+	// Error
+	const error = $("p.error").get(0);
+	if (error) {
+		Swal.fire({
+			title: "Error!",
+			html: "Your attendance didn't register! <br/><br/>"+error.innerText,
+			icon: "error",
+			showConfirmButton: true,
+			timer: 1500,
+			confirmButtonColor: '#3085d6',
+			confirmButtonText: "Ok."
+		})
+	}
 });
